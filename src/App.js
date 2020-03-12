2a0167e27e3d4e1427b44e7c3801a794d6d5d2f3
sagar shah <sagar1189shah@gmail.com>
2020-03-12 15:23:04 +0530

    finish week2 assignment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Main from "./components/MainComponent";
+import { DISHES } from "./shared/dishes";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-      <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div className="App">
+            <Main />
           </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
